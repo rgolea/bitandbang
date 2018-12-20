de48f350b388a637bd28b22c342816390795836b
Rares Golea <rgolea@gmail.com>
2018-12-20 21:33:05 +0100

    Added my card

--- bin/card.js
+++ bin/card.js
@@ -14,19 +14,17 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Rares Golea /'),
+  handle: chalk.cyan('@rgolea'),
+  work: chalk.white('Senior Developer at Eatappi'),
+  twitter: chalk.cyan('https://twitter.com/rgolea'),
+  github: chalk.cyan('https://github.com/rgolea'),
+  linkedin: chalk.cyan('https://linkedin.com/in/rgolea'),
+  npx: chalk.white('npx rgolea'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
   labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:')
 }
 
@@ -37,10 +35,9 @@ const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + newline + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
